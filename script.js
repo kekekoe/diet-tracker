3eb73dafc664651fc85e024b2459db4979f7d4fe
riri <85736640+kekekoe@users.noreply.github.com>
2024-08-30 20:01:38 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -34,32 +34,54 @@ function updateDailyTracker() {
 
     dailyTrackerContainer.innerHTML = '';
 
-    Object.keys(savedEntries).forEach(date => {
+    const sortedDates = Object.keys(savedEntries).sort((a, b) => new Date(b) - new Date(a));
+
+    sortedDates.forEach(date => {
         const dateHeader = document.createElement('p');
-        dateHeader.innerText = `Date: ${date}`;
+        dateHeader.innerText = `${date}`;
         dateHeader.style.fontWeight = 'bold';
+        dateHeader.className = 'dateHeader'; 
+
         dailyTrackerContainer.appendChild(dateHeader);
 
-        savedEntries[date].forEach((entry, index) => {
+        const dateEntriesContainer = document.createElement('div');
+        dateEntriesContainer.className = 'dateEntriesContainer';
+
+        savedEntries[date].reverse().forEach((entry, index) => {
             const entryContainer = document.createElement('div');
             entryContainer.className = 'entryContainer';
 
-            const p = document.createElement('p');
-            p.innerText = entry.text;
-            entryContainer.appendChild(p);
+            const entryContent = document.createElement('div');
+            entryContent.className = 'entryContent';
+            entryContent.innerHTML = `
+                <p>${entry.text}</p>
+            `;
 
-            const editButton = document.createElement('button');
-            editButton.innerText = 'Edit';
-            editButton.onclick = () => editEntry(date, index, entry.text, entryContainer);
-            entryContainer.appendChild(editButton);
+            const buttonContainer = document.createElement('div');
+            buttonContainer.className = 'buttonContainer';
+            buttonContainer.innerHTML = `
+                <button class="editButton">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+                        <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/>
+                    </svg>
+                </button>
+                <button class="deleteButton">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+                        <path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z"/>
+                    </svg>
+                </button>
+            `;
 
-            const deleteButton = document.createElement('button');
-            deleteButton.innerText = 'Delete';
-            deleteButton.onclick = () => deleteEntry(date, index);
-            entryContainer.appendChild(deleteButton);
+            buttonContainer.querySelector('.editButton').onclick = () => editEntry(date, index, entry.text, entryContainer);
+            buttonContainer.querySelector('.deleteButton').onclick = () => deleteEntry(date, index);
 
-            dailyTrackerContainer.appendChild(entryContainer);
+            entryContainer.appendChild(entryContent);
+            entryContainer.appendChild(buttonContainer);
+
+            dateEntriesContainer.appendChild(entryContainer);
         });
+
+        dailyTrackerContainer.appendChild(dateEntriesContainer);
     });
 }
 
