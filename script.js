b54a2c1a22200409979302db14bcc3652b5cc207
riri <85736640+kekekoe@users.noreply.github.com>
2024-09-01 02:23:19 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function updateDailyTracker() {
         months[monthYear].push({ date, entries: savedEntries[date] });
     });
 
-    const sortedMonths = Object.keys(months).sort((a, b) => new Date(b) - new Date(a));
+    const sortedMonths = Object.keys(months).sort((a, b) => new Date(b.replace('-', '/')) - new Date(a.replace('-', '/')));
 
     if (sortedMonths.length === 0) {
         dailyTrackerContainer.innerHTML = '<p>no entries found . . . ໒꒰ྀིっ -｡꒱ྀི১ </p>';
